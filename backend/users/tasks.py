1499bfeccda5d085b4771aab1eff8d2213a7f23a
lwx2018 <40064484+lwx2018@users.noreply.github.com>
2023-12-08 18:07:35 +0800

    Update tasks.py
    delete bug

--- backend/users/tasks.py
+++ backend/users/tasks.py
@@ -1,16 +1,10 @@
 from django.core import management
 
-<<<<<<< HEAD
 from {{project_name}} import celery_app
-=======
 from privacy_computing_demo import celery_app
->>>>>>> b8f188b (增加PIR相关应用)
 
 
 @celery_app.task
 def clearsessions():
-<<<<<<< HEAD
     management.call_command("clearsessions")
-=======
     management.call_command('clearsessions')
->>>>>>> b8f188b (增加PIR相关应用)
