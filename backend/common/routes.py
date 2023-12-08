b98910151f136a52877385e1e69863df73399e26
lwx2018 <40064484+lwx2018@users.noreply.github.com>
2023-12-08 13:31:59 +0800

    Update routes.py
    delete bug

--- backend/common/routes.py
+++ backend/common/routes.py
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 from .views import RestViewSet
 
 routes = [
     {"regex": r"rest", "viewset": RestViewSet, "basename": "Rest"},
-=======
 from .views import RestViewSet, ModelViewSet
 
 routes = [
     {'regex': r'rest', 'viewset': RestViewSet, 'basename': 'Rest'},
     {'regex': r'model', 'viewset': ModelViewSet, 'basename': 'Model'},
->>>>>>> b8f188b (增加PIR相关应用)
 ]
