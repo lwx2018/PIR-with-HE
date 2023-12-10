5575b7a558539cedb09dfa16db62c037dff259a7
lwx2018 <40064484+lwx2018@users.noreply.github.com>
2023-12-10 23:04:24 +0800

    Update index.js
    delete bug

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,30 +1,22 @@
 // import pages
 import * as Sentry from '@sentry/browser';
 import React from 'react';
-<<<<<<< HEAD
 import { createRoot } from 'react-dom/client';
-=======
 import ReactDOM from 'react-dom';
->>>>>>> b8f188b (增加PIR相关应用)
 
 import '../sass/style.scss';
 
 import App from './App';
-<<<<<<< HEAD
-=======
 import ModelInputApp from './ModelInputApp';
 import ModelManageApp from './ModelManageApp';
->>>>>>> b8f188b (增加PIR相关应用)
 
 Sentry.init({
   dsn: window.SENTRY_DSN,
   release: window.COMMIT_SHA,
 });
 
-<<<<<<< HEAD
 const root = createRoot(document.getElementById('react-app'));
 root.render(<App />);
-=======
 if (document.getElementById('react-app') !== null) {
   ReactDOM.render(<App />, document.getElementById('react-app'));
 } else if (document.getElementById('model-input-app') !== null) {
@@ -32,4 +24,3 @@ if (document.getElementById('react-app') !== null) {
 } else if (document.getElementById('model-manage-app') !== null) {
   ReactDOM.render(<ModelManageApp />, document.getElementById('model-manage-app'));
 } 
->>>>>>> b8f188b (增加PIR相关应用)
