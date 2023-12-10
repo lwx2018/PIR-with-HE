16af983602049de526edb9a977acc5bbf1c0ce2d
lwx2018 <40064484+lwx2018@users.noreply.github.com>
2023-12-10 23:35:02 +0800

    Update babel.config.js
    delete bug

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
 'use strict';
 
 module.exports = {
-<<<<<<< HEAD
   presets: ['@babel/preset-env', '@babel/preset-react']
-=======
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: ['react-hot-loader/babel'],
->>>>>>> b8f188b (增加PIR相关应用)
 };
