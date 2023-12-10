e1a67a53909d9aa919e7605b62b1dbaaafd22670
lwx2018 <40064484+lwx2018@users.noreply.github.com>
2023-12-10 23:42:46 +0800

    Update jest-setup.js
    delete bug

--- jest-setup.js
+++ jest-setup.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import '@testing-library/jest-dom';
-=======
 /* eslint-disable import/no-extraneous-dependencies */
 
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 enzyme.configure({ adapter: new Adapter() });
->>>>>>> b8f188b (增加PIR相关应用)
