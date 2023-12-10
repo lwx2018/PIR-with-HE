d2591a2d19c740666551278f05d82586961533b0
lwx2018 <40064484+lwx2018@users.noreply.github.com>
2023-12-10 22:50:32 +0800

    Update Home.js
    delete bug

--- frontend/js/pages/Home.js
+++ frontend/js/pages/Home.js
@@ -3,21 +3,16 @@ import Button from 'react-bootstrap/Button';
 import { useDispatch, useSelector } from 'react-redux';
 
 import DjangoImgSrc from '../../assets/images/django-logo-negative.png';
-<<<<<<< HEAD
 import { fetchRestCheck } from '../store/rest_check';
-=======
+
 import { creators } from '../store/rest_check';
->>>>>>> b8f188b (增加PIR相关应用)
 
 const Home = () => {
   const dispatch = useDispatch();
   const restCheck = useSelector((state) => state.restCheck);
   useEffect(() => {
-<<<<<<< HEAD
     const action = fetchRestCheck();
-=======
     const action = creators.fetchRestCheck();
->>>>>>> b8f188b (增加PIR相关应用)
     dispatch(action);
   }, [dispatch]);
 
@@ -36,11 +31,8 @@ const Home = () => {
         </div>
         <img alt="Django Negative Logo" src={DjangoImgSrc} />
       </div>
-<<<<<<< HEAD
       <div>{restCheck?.data?.payload?.result}</div>
-=======
       <div>{restCheck.result}</div>
->>>>>>> b8f188b (增加PIR相关应用)
       <Button variant="outline-dark" onClick={() => setShowBugComponent(true)}>
         Click to test if Sentry is capturing frontend errors! (Should only work in Production)
       </Button>
